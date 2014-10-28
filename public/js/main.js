081ebb42bdfe37fa57b55f61bdbe0e29bfcf4ed5
Chadtech <chadstearns1@gmail.com>
2014-10-28 12:29:43 -0700

    Cleaned up comments of main.coffee

--- public/js/main.js
+++ public/js/main.js
@@ -1,25 +1,3 @@
-
-/*
-addClass = (elementId, classNamet) ->
-  element = document.getElementById elementId
-  element.className += ' ' + classNamet
-
-removeClass = (elementId, className) ->
-  element = document.getElementById elementId
-  console.log element
-  element.className = 'title'
-
-scrollMenu = (event) ->
-  scrollTop = document.documentElement.scrollTop or document.body.scrollTop
-  if scrollTop > 50
-    addClass 'title', 'scrolled'
-  else
-    console.log 'A'
-    removeClass 'title', 'scrolled'
-
-window.onscroll = scrollMenu
- */
-
 (function() {
   var setClass;
 
@@ -30,7 +8,6 @@ window.onscroll = scrollMenu
   };
 
   window.onmousemove = function(event) {
-    console.log(event.clientX);
     if (event.clientX < 200) {
       setClass('menu', 'menu revealed');
       return setClass('content', 'content revealed');
--- src/js/main.coffee
+++ src/js/main.coffee
@@ -1,30 +1,8 @@
-###
-addClass = (elementId, classNamet) ->
-  element = document.getElementById elementId
-  element.className += ' ' + classNamet
-
-removeClass = (elementId, className) ->
-  element = document.getElementById elementId
-  console.log element
-  element.className = 'title'
-
-scrollMenu = (event) ->
-  scrollTop = document.documentElement.scrollTop or document.body.scrollTop
-  if scrollTop > 50
-    addClass 'title', 'scrolled'
-  else
-    console.log 'A'
-    removeClass 'title', 'scrolled'
-
-window.onscroll = scrollMenu
-###
-
 setClass = (elementId, newClass ) ->
   element = document.getElementById elementId
   element.className = newClass
 
 window.onmousemove = (event) ->
-  console.log event.clientX
   if event.clientX < 200
     setClass 'menu', 'menu revealed'
     setClass 'content', 'content revealed'
